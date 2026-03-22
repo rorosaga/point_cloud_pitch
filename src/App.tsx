e006bddedbbab8a5c464d2fd6940de1d97c67b51
Rodrigo Sagastegui <rodrigo.sagastegui9@gmail.com>
2026-03-22 13:44:41 +0100

    cors

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ export default function App() {
         pointerEvents: 'none',
       }}>
         <img
-          src="http://localhost:8080/stream"
+          src="/stream"
           alt="Video stream"
           style={{
             display: 'block',
